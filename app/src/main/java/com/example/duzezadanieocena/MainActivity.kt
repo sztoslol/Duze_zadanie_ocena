22c8fd6fb0f9442d6953b3ae7be79fe4562075b3
SztosPV <109919302+SztosPV@users.noreply.github.com>
2023-03-30 16:52:28 +0200

    Dodano obsługę przejść między activity

--- app/src/main/java/com/example/duzezadanieocena/MainActivity.kt
+++ app/src/main/java/com/example/duzezadanieocena/MainActivity.kt
@@ -1,33 +1,59 @@
 package com.example.duzezadanieocena
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.TransitionInflater
+import android.view.View
 import android.view.animation.AnimationUtils
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityOptionsCompat
-import android.transition.TransitionInflater
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.button.MaterialButtonToggleGroup
 import com.google.android.material.navigation.NavigationView
 
+
 class MainActivity : AppCompatActivity() {
+
+    private var isNavOpen = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         supportActionBar?.hide() // ukrycie defaultowego topbara
 
-        // Obsługa animacji przejścia do nowego Activity z animacją
-        findViewById<Button>(R.id.button).setOnClickListener {
-            val slideOutRight = TransitionInflater.from(this).inflateTransition(android.R.transition.slide_right)
-            slideOutRight.duration = 750
-            window.exitTransition = slideOutRight
-
-            val slideInLeft = AnimationUtils.loadAnimation(this, R.anim.slide_in_left)
-            val options = ActivityOptionsCompat.makeCustomAnimation(this, R.anim.slide_in_left, R.anim.slide_out_right)
-            val intent = Intent(this, Informacje_o_uzytkowniku::class.java)
-            startActivity(intent, options.toBundle())
+        // Obsługa chowania się manu nawigacji oraz jego animowanie
+        findViewById<Button>(R.id.Button_nav_close).setOnClickListener {
+            val navigationView = findViewById<NavigationView>(R.id.nav_view)
+            val animation = AnimationUtils.loadAnimation(this, R.anim.slide_out_left)
+            navigationView.startAnimation(animation)
+            navigationView.visibility = View.GONE
         }
+
+        // Obsługa pokazywania się manu nawigacji oraz jego animowanie
+        findViewById<ImageView>(R.id.button_hamburger_menu).setOnClickListener {
+            val navigationView = findViewById<NavigationView>(R.id.nav_view)
+            val animation = AnimationUtils.loadAnimation(this, R.anim.slide_in_left)
+            navigationView.startAnimation(animation)
+            navigationView.visibility = View.VISIBLE
+        }
+
+        // Obsługa przejść pomiędzy activity
+        findViewById<Button>(R.id.Button_nav_homepage).setOnClickListener {
+            Toast.makeText(this, "Jesteś już na stronie głównej", Toast.LENGTH_SHORT).show()
+        }
+
+        findViewById<Button>(R.id.Button_nav_marks).setOnClickListener {
+            startActivity(Intent(this, tabela_ocen::class.java))
+            overridePendingTransition(R.anim.slide_in_left, R.anim.slide_in_left)
+        }
+
+        findViewById<Button>(R.id.Button_nav_user_info).setOnClickListener {
+            startActivity(Intent(this, Informacje_o_uzytkowniku::class.java))
+            overridePendingTransition(R.anim.slide_in_left, R.anim.slide_in_left)
+        }
+
     }
 }
